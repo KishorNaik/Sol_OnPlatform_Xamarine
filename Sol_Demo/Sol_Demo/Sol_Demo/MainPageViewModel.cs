a53031e6c331a8f533812fb3d15de349baf26713
KishorNaik <kishor.naik011.net@gmail.com>
2020-01-18 18:09:53 +0530

    code commit

---
+++ Sol_Demo/Sol_Demo/Sol_Demo/MainPageViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Sol_Demo
+{
+    public class MainPageViewModel
+    {
+        #region Constructor
+        public MainPageViewModel()
+        {
+            OnSubmitClick();
+        }
+        #endregion
+
+        #region Property
+        public Command OnSubmit { get; set; }
+        #endregion
+
+        #region Method
+        public void OnSubmitClick()
+        {
+            OnSubmit = new Command<ContentPage>((leContentPage) => {
+
+                if (Device.RuntimePlatform == Device.Android)
+                {
+                    var buttonObj = leContentPage.FindByName<Button>("btnSubmit1");
+                    buttonObj.BackgroundColor = Color.Orange;
+                }
+                else if (Device.RuntimePlatform == Device.iOS)
+                { 
+                    var buttonObj = leContentPage.FindByName<Button>("btnSubmit1");
+                    buttonObj.BackgroundColor = Color.Orange;
+                }
+            
+            });
+        }
+        #endregion 
+    }
+}
